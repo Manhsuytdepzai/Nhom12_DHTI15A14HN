4e2113b8172bdcbc6a979eb3f9f9f5d144e7af82
your name <abc@gmail.com>
2024-10-25 00:50:14 +0700

    First Commit
    creat new folder

---
+++ Nhom12_dhti5a14hn/Form5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhom12_dhti5a14hn
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+    }
+}
